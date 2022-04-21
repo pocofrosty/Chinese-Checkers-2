dc24a9e53556bcb3e4e206c23c5253612c3bdbc6
pocofrosty <91432961+pocofrosty@users.noreply.github.com>
2022-04-21 01:17:59 -0400

    Init

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send(err.message)
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+  Routes, Route, Outlet,
+} from 'react-router-dom'
+
+require('dotenv').config()
+
+// eslint-disable-next-line no-undef
+export default App = () => (
+  <div>
+    <Routes path="/" element={<Layout />}>
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  </div>
+)
+
+const Layout = () => (
+  <div>
+    <Outlet />
+  </div>
+)
+
+const ErrorPage = () => (
+  <div>
+    <h1> Invalid Site </h1>
+  </div>
+)
